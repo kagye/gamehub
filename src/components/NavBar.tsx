84e7c4c2e373466834bdb456c46914e3e29ee242
Kofi Agyemang <agyemko@gmail.com>
2023-04-23 10:45:21 +0100

    (fix): style the search bar and add icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,32 @@
-import { HStack, Image, Input } from '@chakra-ui/react';
+import {
+  HStack,
+  Icon,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import { Search2Icon } from '@chakra-ui/icons';
 import logo from '../assets/logo.webp';
 import ColourModeSwitch from './ColourModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack
+      className="navbar"
+      justifyContent="space-between"
+      padding="1em"
+      marginBottom={'1em'}
+    >
       <Image src={logo} boxSize="60px"></Image>
-      <Input placeholder="Search games..."></Input>
-      <ColourModeSwitch></ColourModeSwitch>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<Icon boxSize="1em" as={Search2Icon} />}
+        />
+        <Input borderRadius="2em" placeholder="Search games..."></Input>
+      </InputGroup>
+      <ColourModeSwitch />
     </HStack>
   );
 };

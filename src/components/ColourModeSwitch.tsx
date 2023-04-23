5f3745cf8fc9802602aaea664f8734b9b798466d
Kofi Agyemang <agyemko@gmail.com>
2023-04-23 10:44:37 +0100

    prevent the text from wrapping

--- src/components/ColourModeSwitch.tsx
+++ src/components/ColourModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColourModeSwitch = () => {
         colorScheme="green"
         size="md"
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
